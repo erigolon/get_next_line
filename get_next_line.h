220fa3c818b1c3b777bbfc02010d19ceced8192f
Esteban Rigolon <erigolon@c3r6s4.42malaga.com>
2023-01-30 14:28:01 +0100

    Creación de la base de get_next_line
    Se ha creado los 3 archivos principales que se deberán entregar,
    solo la base de ellos, nada imporatente se ha realizado aún.

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erigolon <erigolon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 10:49:39 by erigolon          #+#    #+#             */
+/*   Updated: 2023/01/23 11:40:48 by erigolon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif

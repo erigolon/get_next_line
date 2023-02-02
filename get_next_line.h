d098e5ac4f8fb69d87b56d940a6d9e921b07970f
Esteban Rigolon <erigolon@c3r9s3.42malaga.com>
2023-02-02 20:50:59 +0100

    Casi finalizado de g.n.l
    El proyecto está 99% finalizado, me queda por tocar unas cosas de
    liberación de memoria que me pide el paco.

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: erigolon <erigolon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 10:49:39 by erigolon          #+#    #+#             */
-/*   Updated: 2023/01/23 11:40:48 by erigolon         ###   ########.fr       */
+/*   Updated: 2023/02/02 20:28:59 by erigolon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 # include <stdlib.h>
 # include <stdio.h>
 # include <unistd.h>
@@ -20,5 +22,9 @@
 # include <fcntl.h>
 
 char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
